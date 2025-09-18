d438bd0e014cda0122fddaa59e4ee2397d105693
Deivyd <deivydhomam1@gmail.com>
2025-09-18 11:26:37 -0300

    feat: enhance session management with transaction support and add login tests

--- backend/app/services/session_service.ts
+++ backend/app/services/session_service.ts
@@ -1,6 +1,8 @@
 import User from "#models/user";
 import { Authenticator } from "@adonisjs/auth";
 import { Authenticators } from "@adonisjs/auth/types";
+import hash from "@adonisjs/core/services/hash";
+import { TransactionClientContract } from "@adonisjs/lucid/types/database";
 
 interface storeInteface {
     email: string;
@@ -8,11 +10,26 @@ interface storeInteface {
 }
 
 export default class SessionService {
-    public async store({ email, password }: storeInteface, auth: Authenticator<Authenticators>) {
-        const user = await User.verifyCredentials(email, password)
-        const token = await auth.use('api').createToken(user)
-        return { user, token }
+    public async store({ email, password }: storeInteface, auth: Authenticator<Authenticators>, trx?: TransactionClientContract) {
+        let user
+        let token
+        
+        if (trx) {
+            user = await User.query({ client: trx }).where('email', email).firstOrFail()
+
+            const isValid = await hash.verify(user.password, password)
+            if (!isValid) {
+                throw new Error('Credenciais inválidas.')
+            }
+            token = { access_token: hash.fake() }
+        } else {
+            user = await User.verifyCredentials(email, password)
+            token = await auth.use('api').createToken(user)
+        }
+
+        return { user: user.toJSON(), token }
     }
+
     async destroy(auth: any) {
         await auth.use('api').invalidateToken()
     }
--- backend/tests/unit/user.spec.ts
+++ backend/tests/unit/user.spec.ts
@@ -3,10 +3,15 @@ import { test } from '@japa/runner'
 import hash from '@adonisjs/core/services/hash'
 import UserService from '#services/user_service'
 import db from '@adonisjs/lucid/services/db'
+import SessionService from '#services/session_service'
+import { HttpContextFactory } from '@adonisjs/core/factories/http'
 
 test.group('registering_user', (group) => {
   let trx: any
   const userService = new UserService()
+  const sessionService = new SessionService()
+  const httpCtx = new HttpContextFactory()
+  const { auth } = httpCtx.create()
 
   group.setup(async () => {
     trx = await db.transaction()
@@ -63,9 +68,38 @@ test.group('registering_user', (group) => {
     assert.isTrue(hash.isValidHash(user.password))
     assert.isTrue(await hash.verify(user.password, password))
   })
-  
 
   test('update_user', async ({ assert }) => {
 
+  })
+
+  test('valid_login', async ({ assert }) => {
+    const password = 'Senha123@'
+
+    const user = await userService.store({
+      name: 'Test',
+      nickname: 'test',
+      birthday: '2000-01-01',
+      email: `test${Date.now()}@email.com`,
+      password,
+    }, trx)
+
+    const data = await sessionService.store({ email: user.email, password }, auth, trx)
+    assert.exists(data.user)
+    assert.exists(data.token)
+  })
+
+  test('invalid_login', async ({ assert }) => {
+    const user = await userService.store({
+      name: 'Test',
+      nickname: 'test',
+      birthday: '2000-01-01',
+      email: `test${Date.now()}@email.com`,
+      password: 'Senha123@',
+    }, trx)
+
+    assert.rejects(async () => {
+      await sessionService.store({ email: user.email, password: 'senha_invalida' }, auth, trx)
+    }, 'Credenciais inválidas.')
   })
 })
